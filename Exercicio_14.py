151cb1ba578fb820b84a8e8d429ea8d871dfb61f
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-08-26 01:03:00 -0300

    Create Exercicio_14.py

---
+++ Exercicio_14.py
@@ -0,0 +1,9 @@
+numero_1 = int(input('Número 1: '))
+numero_2 = int(input('Número 2: '))
+
+soma = numero_1 + numero_2
+sub = numero_1 - numero_2
+multi = numero_1 * numero_2
+divi = numero_1 / numero_2
+
+print(soma, sub, multi, divi)

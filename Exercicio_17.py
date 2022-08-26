f74bf1395822d1223a368978a49115ac3d98b84d
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-08-26 01:03:10 -0300

    Create Exercicio_17.py

---
+++ Exercicio_17.py
@@ -0,0 +1,10 @@
+numero_1 = int(input('Número 1: '))
+numero_2 = int(input('Número 2: '))
+numero_3 = int(input('Número 3: '))
+peso_1 = int(input('Peso 1: '))
+peso_2 = int(input('Peso 2: '))
+peso_3 = int(input('Peso 3: '))
+
+valor = numero_1 / peso_1 + numero_2 / peso_2 + numero_3 / peso_3
+
+print(valor)

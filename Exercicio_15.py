83cc390920fee0fc32561806aba8d91b1839440a
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-08-26 01:03:04 -0300

    Create Exercicio_15.py

---
+++ Exercicio_15.py
@@ -0,0 +1,8 @@
+numero_1 = int(input('Número 1: '))
+numero_2 = int(input('Número 2: '))
+numero_3 = int(input('Número 3: '))
+numero_4 = int(input('Número 4: '))
+
+soma = numero_1 + numero_2 + numero_3 + numero_4
+
+print(soma)

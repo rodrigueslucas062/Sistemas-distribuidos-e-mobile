9f6e7f75fc7733b17aa75180a276f00de7764ee7
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-08-27 01:39:04 -0300

    Create Exercicio_25.py

---
+++ Exercicio_25.py
@@ -0,0 +1,7 @@
+#Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que:
+
+base = int(input('Base: '))
+altura = int(input('Altura: '))
+
+area = (base * altura) / 2 
+print('A área é: {:.1f}'.format(area))

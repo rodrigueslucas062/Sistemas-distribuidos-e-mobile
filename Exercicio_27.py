ddb8b88dee85f6a6237198703890b31d84253948
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-08-28 18:48:04 -0300

    Update Exercicio_27.py

--- Exercicio_27.py
+++ Exercicio_27.py
@@ -1,12 +1,13 @@
+#Faça um programa que receba um número positivo e maior que zero, calcule e mostre
 
 import math
 numero = float(input('Número: '))
 
-if numero < 0:
-    print('O número não pode ser menor que 0')
-
-quadrado = numero **2
-cubo = numero **3
-raiz = math.sqrt(numero)
+if numero > 0:
+    quadrado = numero **2
+    cubo = numero **3
+    raiz = math.sqrt(numero)
+else:
+    print('O numero precisa ser maior que zero')
 
 print(quadrado, cubo, raiz)

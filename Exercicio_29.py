c571d7b4667d89cb93e53568f67d8fde00daab34
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-08-28 19:35:15 -0300

    Create Exercicio_29.py

---
+++ Exercicio_29.py
@@ -0,0 +1,11 @@
+#Sabe-se que: pé = 12 polegadas; 1 jarda = 3 pés e 1 milha = 1,760 jarda. Faça um programa que receba uma medida em pés, faça as conversões a seguir e mostre os resultados.
+
+pes = float(input('Pés: '))
+
+jarda = pes / 3
+polegada = pes * 12
+milha = pes/ 5280
+
+conversao = (jarda, polegada,  milha)
+
+print(conversao)

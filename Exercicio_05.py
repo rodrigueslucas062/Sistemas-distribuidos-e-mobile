b2c1bc8f49ba712417f93c435baf5da4b2da9fa1
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-08-28 22:22:59 -0300

    Update Exercicio_05.py

--- Exercicio_05.py
+++ Exercicio_05.py
@@ -1,3 +1,2 @@
-#A)
-x = 2
-print(“x = ”) + (x)
+#O que aparece na janela do console, quando cada uma das instruções abaixo são executadas, para: x = 2 e y = 3? Execute cada uma das linhas abaixos e, se necessário, faça os devidos ajustes no código.
+

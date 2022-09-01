6cd011f229c3963670df79ceae9b306457691018
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-09-01 00:00:01 -0300

    Update Exercicio_05.py

--- Exercicio_05.py
+++ Exercicio_05.py
@@ -1,2 +1,8 @@
 #O que aparece na janela do console, quando cada uma das instruções abaixo são executadas, para: x = 2 e y = 3? Execute cada uma das linhas abaixos e, se necessário, faça os devidos ajustes no código.
 
+x = 2
+y = 3 
+print(f'x = {x}')
+print(f'O valor de x + x é {x + x}')
+print(f'y = {y}')
+print(f'x + y = {x + y}')

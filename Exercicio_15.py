3acb126a4e4d346e58a5b5d1f65658dde6506d2e
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-09-01 00:00:03 -0300

    Update Exercicio_15.py

--- Exercicio_15.py
+++ Exercicio_15.py
@@ -1,1 +1,10 @@
 #Faça um programa que leia dois números inteiros e imprima o dividendo, divisor, quociente e resto da divisão
+
+dividendo = int(input("Número 1:"))
+divisor = int(input('Número 2: '))
+
+quociente = dividendo/divisor
+
+if quociente != 0:
+    resto = dividendo % divisor
+print(f'Dividendo: {dividendo}, Divisor {divisor}, Quociente {quociente}, Resto {resto}')
